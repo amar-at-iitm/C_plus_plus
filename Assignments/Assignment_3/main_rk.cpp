c202b80e1c02086276c6eeff6e82c2a907f3822b
Amar <ma24m002@smail.iitm.ac.in>
2024-11-05 01:00:01 +0530

    Assignment 3

---
+++ Assignments/Assignment_3/main_rk.cpp
@@ -0,0 +1,70 @@
+#include "AbstractOdeSolver.hpp"
+#include "RK_4th_order.hpp"
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+
+int main(){
+    double y=2.0,t0=0.0, tn=1.0, h1=0.1, h2=0.05;
+    /*
+     cout<<"Enter initial vaule of y "<<endl;
+    cin>>y;
+    cout<<"Enter initial time t0 "<<endl;
+    cin>>t0;
+    cout<<"Enter step sizes h1 and h2 "<<endl;
+    cin>>h1>>h2;
+    cout<<"Enter final time tn "<<endl;
+    cin>>tn;
+    */
+    int n=(tn - t0)/h1;
+   
+    RK_4th_order rk1;
+    rk1.SetStepSize(h1);         
+    rk1.SetTimeInterval(t0, tn);
+    rk1.SetInitialValue(y);
+ 
+    string filename = "rk_output1.csv";     // Filename for output 
+    ofstream outFile(filename);             // Opening file to write the results
+    if (!outFile.is_open()) {
+        cerr << "Error opening file!" << endl;
+        exit(1);
+    }
+    
+    outFile << "t, y\n";                    // Write the initial values to file
+    outFile << t0 << ", " << y << "\n";
+    for(int i=0; i<n; i++)
+    {
+        y = rk1.SolveEquation();
+        t0 = t0 + h1;
+        outFile << t0 << ", " << y << endl;
+    }    
+    outFile.close(); 
+///////////////////////////////////////////////////////////////////////////////////////////////
+
+
+    y=2.0,t0=0.0, tn=1.0, h2=0.05;
+    int m=(tn - t0)/h2;
+    RK_4th_order rk2;
+    rk2.SetStepSize(h2);         
+    rk2.SetTimeInterval(t0, tn);
+    rk2.SetInitialValue(y);
+ 
+    filename = "rk_output2.csv";     // Filename for output 
+    ofstream outFile1(filename);             // Opening file to write the results
+    if (!outFile1.is_open()) {
+        cerr << "Error opening file!" << endl;
+        exit(1);
+    }
+    
+    outFile1 << "t, y\n";                    // Write the initial values to file
+    outFile1 << t0 << ", " << y << "\n";
+    for(int i=0; i<m; i++)
+    {
+        y = rk2.SolveEquation();
+        t0 = t0 + h2;
+        outFile1 << t0 << ", " << y << endl;
+    }    
+    outFile1.close(); 
+    return 0;
+}
